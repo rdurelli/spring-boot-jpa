fc9fb26e9a3b750990a1362a7aac849d98cd1f46
Rafael Durelli <rafael.durelli@dcc.ufla.br>
2019-04-17 18:19:00 -0300

    small

--- src/main/java/com/rafaeldurelli/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafaeldurelli/cursomc/cursomc/resources/CategoriaResource.java
@@ -4,15 +4,12 @@ package com.rafaeldurelli.cursomc.cursomc.resources;
 import com.rafaeldurelli.cursomc.cursomc.domain.Categoria;
 import com.rafaeldurelli.cursomc.cursomc.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
